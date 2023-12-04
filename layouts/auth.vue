7488ad8fbc817a921384475c43dd2f93cb481e4a
Raihan R <raihanryuichi@gmail.com>
2023-12-05 05:41:52 +0700

    layouts: update auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-    <div class="grid grid-cols-12 p-8 md:p-12 lg:p-16 h-[100vh] bg-slate-950">
-        <div class="col-span-12 sm:col-start-3 sm:col-span-8 md:col-start-4 md:col-span-6 lg:col-start-5 lg:col-span-4">
+    <div class="grid grid-cols-12 p-12 lg:p-24 h-[100vh] bg-slate-950">
+        <div class="col-span-12 sm:col-start-3 sm:col-span-8 md:col-start-4 md:col-span-6 xl:col-start-5 xl:col-span-4">
             <slot />
         </div>
     </div>
